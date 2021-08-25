a991cb80a4f7c31b9aa5eeb2be2383f39a53674c
ALCintra <89037240+ALCintra@users.noreply.github.com>
2021-08-25 13:49:21 -0300

    Delete 01-) Fase Um directory

--- 01-) Fase Um/16-) Carta de Amor.js
+++
@@ -1,3 +0,0 @@
-
-
-console.log(" Tudo o essas poucas palavras entrem em seu coração e façam você entender que eu mais queria\n nesse momento era conseguir escrever uma carta de amor.\n No entanto, eu não consigo expressar tudo o que sinto por você.\n O quão significante és para mim, que quero ser feliz o resto de minha vida com você\n e que nada e nem ninguém poderá nos separar.\n Eu te amo!");
